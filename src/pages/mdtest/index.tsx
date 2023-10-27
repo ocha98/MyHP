af0d4b828f4d61d0baad565e65e381ced32e1fd6
ocha98 <48848793+ocha98@users.noreply.github.com>
2023-10-27 15:45:08 +0900

    Markdown test page

---
+++ src/pages/mdtest/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Codemirror from '@uiw/react-codemirror'
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data'
+import MDtoHTML from 'lib/MDtoHTML';
+import StyleMD from "../../styles/Markdown.module.scss"
+
+const App = () => {
+  const [markdownText, setMarkdownText] = useState('');
+  const [htmlText, setHtmlText] = useState('');
+  useEffect(() => {
+    MDtoHTML(markdownText).then(s => setHtmlText(s));
+  }, [markdownText]);
+
+  return (
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ flex: 1, overflow: 'auto' }}>
+        <Codemirror 
+          extensions={[markdown({base:markdownLanguage, codeLanguages: languages})]} 
+          onChange={(value, viewUpdate) => {setMarkdownText(value)}}
+        />
+      </div>
+      <div style={{ flex: 1, overflow: 'auto', padding: '1em', borderLeft: "1px solid gray" }}>
+        <div className={StyleMD.MD_container} dangerouslySetInnerHTML={{ __html: htmlText }}/>
+      </div>
+    </div>
+  );
+};
+
+export default App;
