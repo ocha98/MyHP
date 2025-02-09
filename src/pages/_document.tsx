1b5ab90c80722bbefe1a6245394eedd332a4fadc
ocha98 <48848793+ocha98@users.noreply.github.com>
2025-02-09 11:37:34 +0900

    adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='ja'>
-      <Head />
+      <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8825122287641994" crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
