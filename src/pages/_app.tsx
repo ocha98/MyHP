7880f3377e045ca13b62b9a4795c8eda69d38fe0
ocha98 <48848793+ocha98@users.noreply.github.com>
2025-02-09 13:47:26 +0900

    [Fix] adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,24 @@ import 'katex/dist/katex.min.css'
 import { BIZ_UDPGothic } from 'next/font/google'
 import Header from 'components/organism/Header'
 import Footer from 'components/organism/Footer'
+import Script from 'next/script'
+import { createContext, useState } from 'react'
 
 const biz = BIZ_UDPGothic({ weight:["400", "700"], subsets: ['latin']})
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export const AdSenseContext = createContext<boolean>(false);
 
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [adsLoaded, setAdsLoaded] = useState(false);
   return(
-    <>
+    <AdSenseContext.Provider value={adsLoaded}>
+      <Script
+        src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8825122287641994"
+        crossOrigin = "anonymous"
+        strategy = "afterInteractive"
+        onLoad={() => setAdsLoaded(true)}
+      />
+
       <style jsx global>{`
         html {
           font-family: ${biz.style.fontFamily};
@@ -22,6 +33,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <Header />
           <Component {...pageProps} />
         <Footer />
-    </>
+    </AdSenseContext.Provider>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang='ja'>
       <Head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8825122287641994" crossOrigin="anonymous"></script>
       </Head>
       <body>
         <Main />
